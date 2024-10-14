901a425a0e4470aaeef8b6df0ad94f30edf1be88
Miguel Marcelino <mac.marcelino@outlook.pt>
2024-10-14 21:58:33 +0100

    feature: Implement language selection as dropdown

--- src/components/i18n/I18n.jsx
+++ src/components/i18n/I18n.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useTranslation } from "react-i18next";
+import { ptFlag, usaFlag, world } from "../../assets";
 
 const I18n = () => {
   const { i18n } = useTranslation();
@@ -8,40 +9,20 @@ const I18n = () => {
     i18n.changeLanguage(language);
   }
 
-  const selectedLanguage = i18n.language;
   return (
-    <div className="flags-container">
-      <div className="switch">
-        <input
-          id="language-toggle"
-          className="check-toggle check-toggle-round-flat"
-          type="checkbox"
-          checked={selectedLanguage === "pt-PT"}
-          onChange={() =>
-            handleChangeLanguage(
-              selectedLanguage === "pt-PT" ? "en-US" : "pt-PT"
-            )
-          }
-        />
-        <label htmlFor="language-toggle"></label>
-        <span
-          className="on"
-          onClick={() => handleChangeLanguage("en-US")}
-          style={{
-            fontWeight: selectedLanguage === "en-US" ? "bold" : "normal",
-          }}
-        >
-          EN
-        </span>
-        <span
-          className="off"
-          onClick={() => handleChangeLanguage("pt-PT")}
-          style={{
-            fontWeight: selectedLanguage === "pt-PT" ? "bold" : "normal",
-          }}
-        >
-          PT
-        </span>
+    <div className="dropdown">
+      <button className="dropbtn">
+        <img src={world} alt="flag" className="world_icon" />
+      </button>
+      <div className="dropdown-content">
+        <a className="language_elem" onClick={() => handleChangeLanguage("en-US")}>
+          <img src={usaFlag} alt="English" className="flag-icon" />
+          <span className="language-text">EN</span>
+        </a>
+        <a className="language_elem" onClick={() => handleChangeLanguage("pt-PT")}>
+          <img src={ptFlag} alt="Portuguese" className="flag-icon" />
+          <span className="language-text">PT</span>
+        </a>
       </div>
     </div>
   );
